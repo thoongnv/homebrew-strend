3bd8304d8eb70e3aad8907e728e9390d4078ea9f
Thong Nguyen <thoongnv@gmail.com>
2023-08-23 12:26:31 +0700

    Remove spaces

--- Formula/strend.rb
+++ Formula/strend.rb
@@ -4,14 +4,14 @@ class Strend < Formula
     url "https://github.com/thoongnv/trends-rs/archive/0.2.1.tar.gz"
     sha256 "7b75ae29222344f674801d794f29c063971088c4cd50bfd2af74b562f1c6a98e"
     license "MIT"
-  
+
     depends_on "rust" => :build
-  
+
     def install
-      system "cargo", "install", *std_cargo_args
+        system "cargo", "install", *std_cargo_args
     end
-  
+
     test do
-      assert_match version.to_s, shell_output("#{bin}/strend --version")
+        assert_match version.to_s, shell_output("#{bin}/strend --version")
     end
-  end
+end
